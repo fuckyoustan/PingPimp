bee72c8f6c40ef2c31d0e2f3b461cfca8eaf42d1
STAN <95098175+fuckyoustan@users.noreply.github.com>
2026-02-23 16:14:53 +0700

    Delete action.sh

--- action.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-if pm list packages | grep -q "webui"; then
-	am start -n io.github.a13e300.ksuwebui/.WebUIActivity --es id "PingPimp"
-else
-	echo "KSU WebUI Standalone is not installed"
-fi
